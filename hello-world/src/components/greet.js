0ae74ca40d586ce84d5055de720dd4f6a9f04daf
RAYAPATI DIVYASREE <divyasailu01@gmail.com>
2024-04-21 12:09:00 +0530

    class components

--- hello-world/src/components/greet.js
+++ hello-world/src/components/greet.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function Greet() {
-  return <h1>Hi Divyas</h1>;
-}
+// function Greet() {
+//   return <h1>Hi Divyas</h1>;
+// }
+
+const Greet = () => <h1>I have used arrow functions</h1>;
+export const meeting = () => <h2>We have meeting at 10Pm</h2>;
 
 export default Greet;
